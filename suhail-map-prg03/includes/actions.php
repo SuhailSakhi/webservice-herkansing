64a8cef680626ed1386665a39ab2fcfdf1b646bc
SuhailSakhi <suhail_909@outlook.com>
2024-03-14 18:23:20 +0100

    herkansing 2

--- suhail-map-prg03/includes/actions.php
+++ suhail-map-prg03/includes/actions.php
@@ -2,138 +2,225 @@
 /**
  * @return array
  */
-function getMusea()
+function getFighter()
 {
-    return [
-        [
-            "id" => 1,
-            "name" => "Maritiem Museum",
-        ],
-        [
-            "id" => 2,
-            "name" => "Depot van Boojmans van Beuningen",
-        ],
-        [
-            "id" => 3,
-            "name" => "Mariniersmuseum",
-        ],
-    ];
+      return [
+          [
+              "id" => 1,
+              "name" => "Khabib Nurmagomedov",
+          ],
+          [
+              "id" => 2,
+              "name" => "Dustin Poirier",
+          ],
+          [
+              "id" => 3,
+              "name" => "Conor Mcgregor",
+
+          ],
+          [
+              "id" => 4,
+              "name" => "Isreal Adesanya",
+          ],
+
+          [
+              "id" => 5,
+              "name" => "Alexander Volkanovski",
+
+          ],
+          [
+              "id" => 6,
+              "name" => "Max Holloway",
+          ],
+          [
+              "id" => 7,
+              "name" => "Kamaru Usman",
+          ],
+          [
+              "id" => 8,
+              "name" => "Alex Perreira",
+          ],
+
+          [
+              "id" => 9,
+              "name" => "Jon jones",
+          ],
+
+          [
+              "id" => 10,
+              "name" => "Francis Ngannou",
+          ]
+      ];
 }
 
 /**
  * @param $id
  * @return mixed
  */
-function getMuseaDetails($id)
+function getFighterDetails($id)
 {
     $tags = [
         1 => [
             "id" => 1,
-            "name" => "Maritiem Museum",
-            "img" => "img/maritiem.jpeg",
-            "imgalt" => "maritiem logo",
-            "linktext" => "maritiemmuseum.nl",
-            "link" => "https://maritiemmuseum.nl/",
-            "criteria" => [
-                "parkeren: Ja",
-                "invalide toilet: Ja",
-                "entree toegankelijkheidvriendelijk: Ja. ",
-                "invalide toilet: Ja",
+            "name" => "Khabib Nurmagomedov",
+            "img" => "img/khabib.jpg",
+            "imgalt" => "khabib logo",
+            "bio" => [
+                "record: 29-0",
+                "weightclass: Lightweight",
+                "reach: 70 inches",
+                "age: 32",
+                "height: 5'10",
+                "born: September 20, 1988",
+                "fighting out of: Dagestan, Russia",
+                "style: Sambo, Judo, Wrestling",
+            ],
+        ],
+            2 => [
+                "id" => 2,
+                "name" => "Dustin Poirier",
+                "img" => "img/dustin.jpg",
+                "imgalt" => "Dustin Poirier",
+                "bio" => [
+                    "record: 28-7 (1 NC)",
+                    "weightclass: Lightweight",
+                    "reach: 72 inches",
+                    "age: 33",
+                    "height: 5'9",
+                    "born: January 19, 1989",
+                    "fighting out of: Lafayette, Louisiana, United States",
+                    "style: Boxing, Brazilian Jiu-Jitsu, Wrestling",
+                ],
+            ],
+            3 => [
+                "id" => 3,
+                "name" => "Conor McGregor",
+                "img" => "img/conor.jpg",
+                "imgalt" => "Conor McGregor",
+                "bio" => [
+                    "record: 22-6",
+                    "weightclass: Lightweight, Welterweight",
+                    "reach: 74 inches",
+                    "age: 33",
+                    "height: 5'9",
+                    "born: July 14, 1988",
+                    "fighting out of: Dublin, Ireland",
+                    "style: Boxing, Taekwondo",
+                ],
+        ],
+        4 => [
+            "id" => 4,
+            "name" => "Israel Adesanya",
+            "img" => "img/isreal.jpg",
+            "imgalt" => "israel logo",
+            "bio" => [
+                "record: 20-1",
+                "weightclass: Middleweight",
+                "reach: 80 inches",
+                "age: 32",
+                "height: 6'4",
+                "born: July 22, 1989",
+                "fighting out of: Auckland, New Zealand",
+                "style: Kickboxing, Boxing, Taekwondo",
+            ],
+        ],
+        5 => [
+            "id" => 5,
+            "name" => "Alexander Volkanovski",
+            "img" => "img/alex.jpg",
+            "imgalt" => "alexander logo",
+            "bio" => [
+                "record: 23-1",
+                "weightclass: Featherweight",
+                "reach: 71 inches",
+                "age: 33",
+                "height: 5'6",
+                "born: September 29, 1988",
+                "fighting out of: New South Wales, Australia",
+                "style: Freestyle Wrestling, Boxing, Brazilian Jiu-Jitsu",
+            ],
+        ],
+        6 => [
+            "id" => 6,
+            "name" => "Max Holloway",
+            "img" => "img/max.jpg",
+            "imgalt" => "max logo",
+            "bio" => [
+                "record: 22-6",
+                "weightclass: Featherweight",
+                "reach: 69 inches",
+                "age: 30",
+                "height: 5'11",
+                "born: December 4, 1991",
+                "fighting out of: Waianae, Hawaii, United States",
+                "style: Boxing, Kickboxing, Brazilian Jiu-Jitsu",
+            ],
+        ],
+        7 => [
+            "id" => 7,
+            "name" => "Kamaru Usman",
+            "img" => "img/kamaru.jpg",
+            "imgalt" => "kamaru logo",
+            "bio" => [
+                "record: 19-1",
+                "weightclass: Welterweight",
+                "reach: 76 inches",
+                "age: 34",
+                "height: 6'0",
+                "born: May 11, 1987",
+                "fighting out of: Auchi, Nigeria",
+                "style: Wrestling, Boxing, Judo",
+            ],
+        ],
+        8 => [
+            "id" => 8,
+            "name" => "Alex Pereira",
+            "img" => "img/alex.jpg",
+            "imgalt" => "Alex Pereira",
+            "bio" => [
+                "record: 3-1",
+                "weightclass: Middleweight",
+                "reach: 75 inches",
+                "age: 34",
+                "height: 6'1",
+                "born: July 25, 1987",
+                "fighting out of: Sao Paulo, Brazil",
+                "style: Kickboxing, Boxing, Brazilian Jiu-Jitsu",
+            ],
+        ],
+        9 => [
+            "id" => 9,
+            "name" => "Jon Jones",
+            "img" => "img/jon.jpg",
+            "imgalt" => "Jon Jones",
+            "bio" => [
+                "record: 26-1 (1 NC)",
+                "weightclass: Light Heavyweight, Heavyweight",
+                "reach: 84.5 inches",
+                "age: 34",
+                "height: 6'4",
+                "born: July 19, 1987",
+                "fighting out of: Albuquerque, New Mexico, United States",
+                "style: Wrestling, Muay Thai, Brazilian Jiu-Jitsu",
+            ],
+        ],
+        10 => [
+            "id" => 10,
+            "name" => "Francis Ngannou",
+            "img" => "img/francis.jpg",
+            "imgalt" => "Francis Ngannou",
+            "bio" => [
+                "record: 16-3",
+                "weightclass: Heavyweight",
+                "reach: 83 inches",
+                "age: 35",
+                "height: 6'4",
+                "born: September 5, 1986",
+                "fighting out of: Paris, France",
+                "style: Boxing, Brazilian Jiu-Jitsu",
+            ],
+        ],
 
-
-            ],
-            "openingstijden" => [
-                "gesloten",
-                "10:00 tot 17:00",
-                "10:00 tot 17:00",
-                "10:00 tot 17:00",
-                "10:00 tot 17:00",
-                "10:00 tot 17:00",
-                "11:00 tot 17:00"
-            ],
-            "openingsdagen" => [
-                "Maandag",
-                "Dinsdag",
-                "Woensdag",
-                "Donderdag",
-                "Vrijdag",
-                "Zondag",
-                "Zaterdag"
-            ],
-            "bannerimg" => "img/maritiem.jpeg",
-            "bannerimgalt" => "banner maritiem",
-            "info" => "Aan de haven van Rotterdam staat het maritiem museum met wisselende, eigentijdse tentoonstellingen en activiteiten neemt het maritiem museum je mee op reis door ons zeevaart heden, verleden en de toekomst."
-        ],
-        2 => [
-            "id" => 2,
-            "name" => "Depot van Boojmans van Beuningen",
-            "img" => "img/boojman.jpg",
-            "imgalt" => "boojman",
-            "linktext" => "boojman.nl",
-            "link" => "https://www.boijmans.nl/nl",
-                "criteria" => [
-                    "parkeren: Ja",
-                    "invalide toilet: Ja",
-                    "entree toegankelijkheidvriendelijk: Ja. ",
-                    "invalide toilet: Ja",
-            ],
-            "openingstijden" => [
-                "Gesloten",
-                "11:00 tot 17:00",
-                "11:00 tot 17:00",
-                "11:00 tot 17:00",
-                "11:00 tot 17:00",
-                "11:00 tot 17:00",
-                "11:00 tot 17:00"
-            ],
-            "openingsdagen" => [
-                "Maandag",
-                "Dinsdag",
-                "Woensdag",
-                "Donderdag",
-                "Vrijdag",
-                "Zondag",
-                "Zaterdag"
-            ],
-            "bannerimg" => "img/boojman.jpg",
-            "bannerimgalt" => "banner booj",
-            "info" => "Het depot van Boojmans en Beuningen is misschien wel een van de meest indrukwekkende gebouwen in Rotterdam. Het depot is de enige toegankelijke kunst depot in de wereld met ruim 152.000 kunstwerken die niet direct tentoongesteld worden en opgeslagen zijn in het museum.",
-        ],
-        3 => [
-            "id" => 3,
-            "name" => "Mariniersmuseum",
-            "img" => "img/mariniers.jpg",
-            "imgalt" => "logo mariniers",
-            "linktext" => "mariniersmuseum.nl",
-            "link" => "https://www.mariniersmuseum.nl/nl",
-            "criteria" => [
-                "parkeren: Ja",
-                "invalide toilet: Ja",
-                "entree toegankelijkheidvriendelijk: Ja. ",
-                "invalide toilet: Ja",
-            ],
-            "openingstijden" => [
-                "Gesloten",
-                "9:30 tot 17:00",
-                "9:30 tot 17:00",
-                "9:30 tot 17:00",
-                "9:30 tot 17:00",
-                "9:30 tot 17:00",
-                "9:30 tot 17:00"
-            ],
-            "openingsdagen" => [
-                "Maandag",
-                "Dinsdag",
-                "Woensdag",
-                "Donderdag",
-                "Vrijdag",
-                "Zondag",
-                "Zaterdag"
-            ],
-            "bannerimg" => "img/mariniers.jpg",
-            "bannerimgalt" => "banner mariniers",
-            "info" => "Het stoerste museum van Nederland. Het maritiem museum is een stoer dagje uit voor jong en oud. In het mariniers museum kan je alles te weten komen over de stoerste mannen en vrouwen uit het Nederlandse leger en ook zelf in de schoenen staan van een marinier door de verschillende activiteiten. Het museum heeft verhalen en tentoongestelde stukken uit verschillende oorlogen en oorlogsmissie ’s die je kan bezichtigen.",
-        ],
     ];
 
     return $tags[$id];
---
+++ suhail-map-prg03/js/fighter.js
@@ -0,0 +1,224 @@
+window.addEventListener('load', init);
+
+const apiUrl = 'http://localhost/webservice/suhail-map-prg03';
+let section;
+let list;
+let saveitems = [];
+let detailpage;
+let fighterData = [];
+
+function init() {
+    list = document.getElementById("content");
+    list.addEventListener('click', ItemWasClicked);
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(parkenSuccesHandler)
+        .catch(ajaxErrorHandler);
+
+    createdetailpagina();
+
+// Voeg event listener toe aan de Saved fighters knop
+    document.querySelector('.navbar-item').addEventListener('click', showSavedFighters);
+}
+function showSavedFighters() {
+    list.innerHTML = ''; // Leeg de huidige inhoud van de lijst
+
+    // Laad en toon de opgeslagen fighters
+    if (localStorage.getItem("savelist-fighter")) {
+        saveitems = JSON.parse(localStorage.getItem("savelist-fighter"));
+        for (let i = 0; i < saveitems.length; i++) {
+            const fighterName = saveitems[i].replace("save ", "");
+            if (fighterData[fighterName]) { // Controleer of de fighter in de data beschikbaar is
+                const newfighterdiv = document.createElement('section');
+                newfighterdiv.classList.add("content-holder");
+                newfighterdiv.dataset.name = fighterData[fighterName].name;
+                newfighterdiv.id = 'fighter_' + fighterData[fighterName].id;
+                list.appendChild(newfighterdiv);
+                detailSuccesHandler(newfighterdiv, fighterData[fighterName]);
+            }
+        }
+    }
+}
+
+
+function parkenSuccesHandler(data) {
+    for (let fighter of data) {
+        const newfighterdiv = document.createElement('section');
+        newfighterdiv.classList.add("content-holder");
+        newfighterdiv.dataset.name = fighter.name;
+        newfighterdiv.id = 'fighter_' + fighter.id; // Set unique ID for each fighter detail page
+        list.appendChild(newfighterdiv);
+
+        fetch(apiUrl + '?id=' + fighter.id)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(fighterDetail => {
+                detailSuccesHandler(newfighterdiv, fighterDetail);
+            })
+            .catch(ajaxErrorHandler);
+    }
+}
+
+
+
+
+// Define the scrollToFighter function
+function scrollToFighter(fighterId) {
+    // Find the fighter's detail page element by its ID
+    const fighterDetail = document.getElementById('fighter_' + fighterId);
+
+    // Scroll to the fighter's detail page using smooth scrolling behavior
+    fighterDetail.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function createdetailpagina() {
+    const detailpageholder = document.createElement('section');
+    detailpageholder.classList.add('detailpageholder');
+    detailpage = document.createElement('section');
+    detailpage.classList.add('detailpage');
+    detailpage.id = 'hide';
+
+    document.body.appendChild(detailpageholder);
+    detailpageholder.appendChild(detailpage);
+    detailpage.addEventListener('click', ItemWasClicked);
+}
+
+function detailSuccesHandler(fighterCard, fighter) {
+    // fighter foto inladen
+    let fighterimg = document.createElement("img");
+    fighterimg.src = fighter.img;
+    fighterimg.alt = fighter.imgalt;
+    fighterimg.classList.add("content-vak");
+    fighterCard.appendChild(fighterimg);
+
+    // create mid content
+    let midcontent = document.createElement('section');
+    midcontent.classList.add('content-vak');
+    fighterCard.appendChild(midcontent);
+
+    // title inladen
+    let title = document.createElement('h2');
+    title.innerHTML = fighter.name;
+    midcontent.appendChild(title);
+
+
+    // create lees button
+    let leesmeerbutton = document.createElement("button");
+    leesmeerbutton.innerHTML = 'info  ' + fighter.name;
+    leesmeerbutton.classList.add("lees");
+    leesmeerbutton.dataset.id = fighter.id;
+    midcontent.appendChild(leesmeerbutton);
+
+    // create save button
+    let savenbutton = document.createElement("button");
+    savenbutton.innerHTML = 'save ' + fighter.name;
+    savenbutton.id = fighter.name;
+    savenbutton.classList.add("save");
+    savenbutton.classList.add("notsaved");
+    midcontent.appendChild(savenbutton);
+
+    // save the fighter info in fighterData
+    fighterData[fighter.id] = fighter;
+
+    loadSaves(fighter.name);
+}
+
+function loadSaves(fightername) {
+    if (localStorage.getItem("savelist-fighter")) {
+        saveitems = JSON.parse(localStorage.getItem("savelist-fighter"));
+        for (let i = 0; i < saveitems.length; i++) {
+            if (saveitems[i] === "save " + fightername) {
+                let button = document.getElementById(fightername);
+                button.classList.add("saved");
+                button.classList.remove("notsaved");
+                button.innerHTML = button.innerHTML.replace("save", "unsave");
+            }
+        }
+    }
+}
+
+
+
+function showSavedPopup(fighterName) {
+    // Creëer een pop-upbericht
+    alert("Je hebt " + fighterName + " opgeslagen!");
+}
+
+function ItemWasClicked(e) {
+    const targetClassList = e.target.classList;
+
+    // Toggle save status
+    if (targetClassList.contains("save")) {
+        if (targetClassList.contains("notsaved")) {
+            targetClassList.replace("notsaved", "saved");
+            saveitems.push(e.target.innerHTML);
+            // Laat een pop-upbericht zien wanneer een fighter wordt opgeslagen
+            showSavedPopup(e.target.innerHTML.replace("save ", ""));
+        } else if (targetClassList.contains("saved")) {
+            targetClassList.replace("saved", "notsaved");
+            const removeIndex = saveitems.indexOf(e.target.innerHTML.replace("unsave", "save"));
+            if (removeIndex !== -1) {
+                saveitems.splice(removeIndex, 1);
+            }
+        }
+        e.target.innerHTML = e.target.innerHTML.includes("save") ? e.target.innerHTML.replace("save", "unsave") : e.target.innerHTML.replace("unsave", "save");
+        localStorage.setItem("savelist-fighter", JSON.stringify(saveitems));
+    }
+
+    // Toggle detail page visibility
+    if (targetClassList.contains("lees")) {
+        detailpage.id = (detailpage.id === "hide") ? "show" : "hide";
+        document.body.classList.toggle("noscroll");
+        showdetail(e.target.dataset.id);
+    }
+}
+function ajaxErrorHandler(error) {
+    console.error(error);
+}
+
+
+// Define the showdetail function
+function showdetail(id) {
+    const detailcontent = document.getElementsByClassName('detailpage')[0];
+    detailcontent.innerHTML = '';
+
+    // create button
+    let closebutton = document.createElement("button");
+    closebutton.innerHTML = 'close';
+    closebutton.classList.add("lees");
+    detailcontent.appendChild(closebutton);
+
+    // create text container
+    let textcontainer = document.createElement("section");
+    textcontainer.classList.add("textcontainer");
+    detailcontent.appendChild(textcontainer);
+
+    let infolistcontainer = document.createElement("section");
+    infolistcontainer.classList.add("infolistcontainer");
+    textcontainer.appendChild(infolistcontainer);
+
+    let infolisth3 = document.createElement("h3");
+    infolisth3.innerHTML = fighterData[id].name;
+    infolistcontainer.appendChild(infolisth3);
+
+// create ul item
+    let infolist = document.createElement("ul");
+    infolistcontainer.appendChild(infolist);
+
+    for (let t = 0; t < 8; t++) {
+        let infolistitem = document.createElement("li");
+        infolistitem.innerHTML = fighterData[id].bio[t];
+        infolist.appendChild(infolistitem);
+    }
+}
